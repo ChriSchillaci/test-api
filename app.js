c2b333352b53fdb4006be28f06ead8c00c82a9d0
ChriSchillaci <chri2001@outlook.com>
2023-07-05 17:28:45 +0200

    feat: added a filter search bar and some minor changes

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 const box = document.getElementById("box");
 
+// const fetchApi = async () => {
+//   const response = await fetch("https://fakestoreapi.com/products");
+//   const data = await response.json();
+//   console.log(data);
+//   const renderCard = () => {
+//     const displayCard = data.map(({ title, image, price }) => {
+//         return `
+//         <div class='card'><div class='title'>${title}</div>
+//                <img src=${image} class='img-product'>
+//                <div class='price'>Price: $${price}</div>
+//                </div>`;
+//       })
+//       .join("");
+//     box.innerHTML = displayCard;
+//   };
+
+//   renderCard();
+// };
+
+// fetchApi();
+
+const renderCard = ({ title, image, price }) => {
+  const productCard = `
+  <div class='card'>
+   <div class='text'>
+    <div class='title'>${title}</div>
+     <div class='price'>Price:$${price}</div>
+     <button class='buy'>Buy</button></div>
+                <img src=${image} class='img-product'>
+                  </div>`;
+  return productCard;
+};
+
 const fetchApi = async () => {
   const response = await fetch("https://fakestoreapi.com/products");
   const data = await response.json();
-  // console.log(data);
-  const renderCard = () => {
-    const displayCard = data.map(({ title, image }) => {
-        return `
-        <div class='card'><div class='title'>${title}</div>
-               <img src=${image} class='img-product'>
-               </div>`;
-      })
-      .join("");
-    box.innerHTML = displayCard;
-  };
+  console.log(data);
 
-  renderCard();
+  const productList = data.map((item) => {
+    return renderCard({
+      title: item.title,
+      image: item.image,
+      price: item.price,
+    });
+  });
+
+  box.innerHTML = productList.join("");
 };
 
 fetchApi();
-
-// const productMock = {
-//   id: 1,
-//   title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//   price: 109.95,
-//   description:
-//     "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//   category: "men's clothing",
-//   image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//   rating: {
-//     rate: 3.9,
-//     count: 120,
-//   },
-// };
-
-// const renderCard = () => {
-//   const { title, image } = productMock;
-//   displayCard =
-//   `<div class='card'>
-//   <p>${title}</p>
-//   <img src=${image} class='img-product' />
-//   </div>`;
-//   box.innerHTML = displayCard;
-// };
-
-// renderCard();
---
+++ search-filter.js
@@ -0,0 +1,17 @@
+const searchBar = document.querySelector(".search");
+
+const filterMethod = () => {
+  const filter = searchBar.value.toLowerCase();
+  const titles = document.querySelectorAll(".title");
+  
+  titles.forEach((el) => {
+    let text = el.textContent;
+    if (text.toLowerCase().includes(filter.toLowerCase())) {
+      el.parentNode.parentNode.style.display = "";
+    } else {
+      el.parentNode.parentNode.style.display = "none";
+    }
+  });
+};
+
+searchBar.addEventListener("input", filterMethod);
